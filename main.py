8abb961d080768f36af8329b97929433e6a54485
Saimbro <mrtanvirahmedsaim@gmail.com>
2024-08-02 20:37:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,42 @@ from datetime import datetime
 from dotenv import load_dotenv
 import datetime
 from bs4 import BeautifulSoup
+import socket
+import sys
+
+
+# specify Host and Port 
+HOST = '0.0.0.0' 
+PORT = 10000
+
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	# With the help of bind() function 
+	# binding host and port
+	soc.bind((HOST, PORT))
+	
+except socket.error as message:
+	
+	# if any error occurs then with the 
+	# help of sys.exit() exit from the program
+	print('Bind failed. Error Code : '
+		+ str(message[0]) + ' Message '
+		+ message[1])
+	sys.exit()
+	
+# print if Socket binding operation completed 
+print('Socket binding operation completed')
+
+# With the help of listening () function
+# starts listening
+soc.listen(9)
+
+conn, address = soc.accept()
+# print the address of connection
+print('Connected with ' + address[0] + ':'
+	+ str(address[1]))
+
 
 message_history = {}
 intents = discord.Intents.all()
